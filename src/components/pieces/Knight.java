b293d5197ec27a5b98e8cba171f7ee9bcf4d660e
Seppe <lenaertssep@gmail.com>
2024-09-23 17:42:27 +0200

    Add Knight moves

--- src/components/pieces/Knight.java
+++ src/components/pieces/Knight.java
@@ -6,4 +6,13 @@ public class Knight extends Piece {
     public Knight(int col, int row, Color color) {
         super(col, row, color);
     }
+
+    @Override
+    public boolean legalMove(int col, int row) {
+        if (Math.abs(preRow - row) == 2 && Math.abs(preCol - col) == 1) {
+            return true;
+        } else {
+            return Math.abs(preRow - row) == 1 && Math.abs(preCol - col) == 2;
+        }
+    }
 }
