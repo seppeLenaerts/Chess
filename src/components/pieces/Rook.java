dec31a333905819f5aef0cb4b114920e87cadf1b
Seppe <lenaertssep@gmail.com>
2024-09-23 17:46:50 +0200

    Add Rook moves

--- src/components/pieces/Rook.java
+++ src/components/pieces/Rook.java
@@ -6,4 +6,13 @@ public class Rook extends Piece {
     public Rook(int col, int row, Color color) {
         super(col, row, color);
     }
+
+    @Override
+    public boolean legalMove(int col, int row) {
+        if (preCol - col != 0 && preRow - row == 0) {
+            return true;
+        } else {
+            return preCol - col == 0 && preRow - row != 0;
+        }
+    }
 }
