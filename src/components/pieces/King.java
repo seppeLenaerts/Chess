2baabdce07349cfd7369514110e3a11f3d589072
Seppe <lenaertssep@gmail.com>
2024-09-23 17:45:07 +0200

    Add King moves

--- src/components/pieces/King.java
+++ src/components/pieces/King.java
@@ -6,4 +6,9 @@ public class King extends Piece {
     public King(int col, int row, Color color) {
         super(col, row, color);
     }
+
+    @Override
+    public boolean legalMove(int col, int row) {
+        return (Math.abs(preCol - col) <= 1 && Math.abs(preRow - row) <= 1);
+    }
 }
