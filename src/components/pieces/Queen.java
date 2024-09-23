171e6e95b8a2c380bd567c44660bc9333f9e86eb
Seppe <lenaertssep@gmail.com>
2024-09-23 19:04:10 +0200

    Add Queen (Bishop + Rook) moves

--- src/components/pieces/Queen.java
+++ src/components/pieces/Queen.java
@@ -8,4 +8,14 @@ public class Queen extends Piece {
         super(col, row, color);
     }
 
+    @Override
+    public boolean legalMove(int col, int row) {
+        if (Math.abs(preCol - col) == Math.abs(preRow - row)) {
+            return true;
+        } else if (preCol - col != 0 && preRow - row == 0) {
+            return true;
+        } else {
+            return preCol - col == 0 && preRow - row != 0;
+        }
+    }
 }
